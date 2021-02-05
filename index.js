fc7c79280e9671528e2dcca1a52582595936792b
niekh1234 <niekhagen@hotmail.com>
2021-02-05 21:13:13 +0100

    axios update & prettier

--- index.js
+++ index.js
@@ -6,10 +6,13 @@ const BASE_URL = 'https://mobileapi.jumbo.com/v12/';
 class JumboApi {
   static getProduct(id) {
     if (typeof id !== 'string')
-      throw new TypeError(`Expected id to be a string, got a ${typeof id}. Format: {number}{containerType}, e.g. 12345PAK`);
+      throw new TypeError(
+        `Expected id to be a string, got a ${typeof id}. Format: {number}{containerType}, e.g. 12345PAK`,
+      );
 
     return new Promise((resolve, reject) => {
-      axios.get(`${BASE_URL}/products/${id}`)
+      axios
+        .get(`${BASE_URL}/products/${id}`)
         .then((res) => {
           resolve(res.data.product.data);
         })
@@ -20,15 +23,15 @@ class JumboApi {
   }
 
   static searchProducts(query, limit, offset) {
-    if (typeof query !== 'string')
-      throw new TypeError(`Expected query to be a string, got a ${typeof query}`);
+    if (typeof query !== 'string') throw new TypeError(`Expected query to be a string, got a ${typeof query}`);
     if (typeof limit !== 'number' && typeof limit !== 'string')
       throw new TypeError(`Expected limit to be a number or a string, got a ${typeof limit}`);
     if (typeof offset !== 'number' && typeof offset !== 'string')
       throw new TypeError(`Expected offset to be a number or a string, got a ${typeof offset}`);
 
     return new Promise((resolve, reject) => {
-      axios.get(`${BASE_URL}/search?q=${query}&limit=${limit}&offset=${offset}`)
+      axios
+        .get(`${BASE_URL}/search?q=${query}&limit=${limit}&offset=${offset}`)
         .then((res) => {
           resolve(res.data.products.data);
         })
@@ -43,7 +46,8 @@ class JumboApi {
       throw new TypeError(`Expected productId to be a string, got a ${typeof productId}`);
 
     return new Promise((resolve, reject) => {
-      axios.get(`${BASE_URL}/search?q=${productId}`)
+      axios
+        .get(`${BASE_URL}/search?q=${productId}`)
         .then((res) => {
           resolve(res.data.products.data[0]);
         })
@@ -55,7 +59,8 @@ class JumboApi {
 
   static getStores() {
     return new Promise((resolve, reject) => {
-      axios.get(`${BASE_URL}/stores`)
+      axios
+        .get(`${BASE_URL}/stores`)
         .then((res) => {
           resolve(res.data.stores.data);
         })
@@ -70,7 +75,8 @@ class JumboApi {
       throw new TypeError(`Expected store id to be a number or a string, got a ${typeof id}`);
 
     return new Promise((resolve, reject) => {
-      axios.get(`${BASE_URL}/stores/${id}`)
+      axios
+        .get(`${BASE_URL}/stores/${id}`)
         .then((res) => {
           resolve(res.data.store.data);
         })
@@ -82,10 +88,13 @@ class JumboApi {
 
   static getStoreDeliveryTimeSlots(id) {
     if (typeof id !== 'number' && typeof id !== 'string')
-      throw new TypeError(`Expected store id to be a number or a string, got a ${typeof id}. For a list of all stores use the getStore() method.`);
+      throw new TypeError(
+        `Expected store id to be a number or a string, got a ${typeof id}. For a list of all stores use the getStore() method.`,
+      );
 
     return new Promise((resolve, reject) => {
-      axios.get(`${BASE_URL}/stores/slots?storeId=${id}&fulfilment=homeDelivery`)
+      axios
+        .get(`${BASE_URL}/stores/slots?storeId=${id}&fulfilment=homeDelivery`)
         .then((res) => {
           resolve(res.data.timeSlots.data);
         })
@@ -100,7 +109,8 @@ class JumboApi {
       throw new TypeError(`Expected id to be a number or a string, got a ${typeof id}`);
 
     return new Promise((resolve, reject) => {
-      axios.get(`${BASE_URL}/stores/slots?storeId=${id}&fulfilment=collection`)
+      axios
+        .get(`${BASE_URL}/stores/slots?storeId=${id}&fulfilment=collection`)
         .then((res) => {
           resolve(res.data.timeSlots.data);
         })
@@ -115,7 +125,8 @@ class JumboApi {
       throw new TypeError(`Expected id to be a number or a string, got a ${typeof id}`);
 
     return new Promise((resolve, reject) => {
-      axios.get(`${BASE_URL}/promotion-overview?store_id=${storeId}`)
+      axios
+        .get(`${BASE_URL}/promotion-overview?store_id=${storeId}`)
         .then((res) => {
           resolve(res.data.tabs);
         })
@@ -126,10 +137,8 @@ class JumboApi {
   }
 
   static getToken(username, password) {
-    if (typeof username !== 'string')
-      throw new TypeError(`Expected username to be a string, got a ${typeof username}`);
-    if (typeof password !== 'string')
-      throw new TypeError(`Expected password to be a string, got a ${typeof password}`);
+    if (typeof username !== 'string') throw new TypeError(`Expected username to be a string, got a ${typeof username}`);
+    if (typeof password !== 'string') throw new TypeError(`Expected password to be a string, got a ${typeof password}`);
 
     const body = {
       username: username,
@@ -139,12 +148,13 @@ class JumboApi {
     const config = {
       headers: {
         'Content-Type': 'application/x-www-form-urlencoded',
-        'User-Agent': 'Jumbo/7.5.2 (python-jumbo-api)'
-      }
+        'User-Agent': 'Jumbo/7.5.2 (python-jumbo-api)',
+      },
     };
 
     return new Promise((resolve, reject) => {
-      axios.post(`${BASE_URL}/users/login`, qs.stringify(body), config)
+      axios
+        .post(`${BASE_URL}/users/login`, qs.stringify(body), config)
         .then((res) => {
           resolve(res.headers['x-jumbo-token']);
         })
@@ -155,21 +165,20 @@ class JumboApi {
   }
 
   static getMyOrders(token) {
-    if (typeof token !== 'string')
-      throw new TypeError(`Expected token to be a string, got a ${typeof token}`);
-    if (token === '')
-      throw new Error('Please provide a token, received nothing');
+    if (typeof token !== 'string') throw new TypeError(`Expected token to be a string, got a ${typeof token}`);
+    if (token === '') throw new Error('Please provide a token, received nothing');
 
     const config = {
       headers: {
         'x-jumbo-token': token,
         'Content-Type': 'application/x-www-form-urlencoded',
-        'User-Agent': 'Jumbo/7.5.2 (python-jumbo-api)'
-      }
+        'User-Agent': 'Jumbo/7.5.2 (python-jumbo-api)',
+      },
     };
 
     return new Promise((resolve, reject) => {
-      axios.get(`${BASE_URL}/users/me/orders`, config)
+      axios
+        .get(`${BASE_URL}/users/me/orders`, config)
         .then((res) => {
           resolve(res.data);
         })
@@ -177,7 +186,7 @@ class JumboApi {
           reject(err);
         });
     });
-  }  
+  }
 }
 
 module.exports = JumboApi;
